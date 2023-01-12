013685dc9ecd4fd02048f7a81258fef2d9a90dc8
xicheng2003 <122506045+xicheng2003@users.noreply.github.com>
2023-01-12 16:37:46 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,16 +51,6 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  navigationLinks: [
-     {
-       title: 'About',
-       pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-     },
-     {
-       title: 'Contact',
-       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-     }
-   ]
   // navigationStyle: 'custom',
   // navigationLinks: [
   //   {
