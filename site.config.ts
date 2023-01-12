a7376a8a53f71a8618fe0782741a2f8ed85fd6e3
xicheng2003 <122506045+xicheng2003@users.noreply.github.com>
2023-01-12 13:20:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e57ccf9dfbcc45aa933e734a4aa1e841',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Morlight Blog',
+  domain: 'https://www.notion.so/e57ccf9dfbcc45aa933e734a4aa1e841',
+  author: 'Michael',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
