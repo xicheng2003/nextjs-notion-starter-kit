58267623bffd4e6f00397ce09e70228925d9c01c
xicheng2003 <122506045+xicheng2003@users.noreply.github.com>
2023-10-04 15:21:35 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright©️2023 {config.author}. All rights reserved. </div>
+      <div className={styles.copyright}>Copyright©️2023 {config.author} All rights reserved. </div>
 
       <div className={styles.settings}>
         {hasMounted && (
