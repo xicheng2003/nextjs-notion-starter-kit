32da3d0ba308a26794c0dfd204817e34a47a83ee
xicheng2003 <122506045+xicheng2003@users.noreply.github.com>
2023-10-08 22:30:04 +0800

    Update PageAside.tsx

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -6,7 +6,7 @@ import { getPageTweet } from '@/lib/get-page-tweet'
 
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
-import { Waline } from './Comment'
+
 
 export const PageAside: React.FC<{
   block: Block
@@ -29,16 +29,4 @@ export const PageAside: React.FC<{
 
   return <PageSocial />
 }
-      {block.id.replace(/-/g, '') !== site.rootNotionPageId ?
-        <Waline
-          serverURL='https://waline.morlight.top'
-          path={'/' + block.id.replace(/-/g, '')}
-          emoji={[
-            '//cdn.jsdelivr.net/gh/walinejs/emojis@1.1.0/tw-emoji'
-          ]}
-          dark={isDarkMode}
-          meta={['nick', 'mail']}
-          requiredMeta={['nick', 'mail']}
-          imageUploader={false}
-          copyright={false}
-        /> : null}
+
